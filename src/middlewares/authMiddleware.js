7526da394b5980f3f6bf3d415d6a46bc584ddfac
John Silveira <john.silveira.pb@compasso.com.br>
2025-05-06 15:35:39 -0300

    refact: ajuste no auth

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,9 +1,16 @@
-exports.verificarLogin = (req, res, next) => {
-  if (req.session.user) return next();
-  res.redirect("/login");
+const verificarLogin = (req, res, next) => {
+  if (!req.session.user) {
+    return res.redirect("/login"); 
+  }
+  next();
 };
 
-exports.verificarAdmin = (req, res, next) => {
-  if (req.session.user?.perfil === "ADMIN") return next();
-  res.status(403).send("Acesso negado");
+
+const verificarAdmin = (req, res, next) => {
+  if (!req.session.user || req.session.user.perfil !== "ADMIN") {
+    return res.status(403).send("Acesso negado."); // Retorna erro se não for admin
+  }
+  next(); 
 };
+
+module.exports = { verificarLogin, verificarAdmin };
